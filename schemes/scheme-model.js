1080d091cb0066d5032e8772d08af0c30eab7f47
Neha Patel <neha.r.p@gmail.com>
2019-09-11 14:32:08 -0700

    created db-config.js and named functions in model

---
+++ schemes/scheme-model.js
@@ -0,0 +1,34 @@
+function find() {
+  //calling find returns promise that resolves to an array of all schemas in db
+}
+
+function findById(id) {
+  //expects a scheme id as parameter
+  //resolve to a single scheme object
+  //on an invalid id, resolves to null
+}
+
+function findSteps(id) {
+  // Expects a scheme id.
+  // Resolves to an array of all correctly ordered step for the given scheme: [ { id: 17, scheme_name: 'Find the Holy Grail', step_number: 1, instructions: 'quest'}, { id: 18, scheme_name: 'Find the Holy Grail', step_number: 2, instructions: '...and quest'}, etc. ].
+  // This array should include the scheme_name not the scheme_id.
+}
+
+function add(scheme) {
+  //     Expects a scheme id.
+  // Resolves to an array of all correctly ordered step for the given scheme: [ { id: 17, scheme_name: 'Find the Holy Grail', step_number: 1, instructions: 'quest'}, { id: 18, scheme_name: 'Find the Holy Grail', step_number: 2, instructions: '...and quest'}, etc. ].
+  // This array should include the scheme_name not the scheme_id.
+}
+
+function update(changes, id) {
+  //     Expects a changes object and an id.
+  // Updates the scheme with the given id.
+  // Resolves to the newly updated scheme object.
+}
+
+function remove(id) {
+  //     Removes the scheme object with the provided id.
+  // Resolves to the removed scheme
+  // Resolves to null on an invalid id.
+  // (Hint: Only worry about removing the scheme. The database is configured to automatically remove all associated steps.)
+}
